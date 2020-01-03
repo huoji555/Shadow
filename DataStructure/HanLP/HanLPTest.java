baddd921bb3ae843158703d593d4893e4e6833f9
huoji555 <huoji566@gmail.com>
2020-01-04 01:14:37 +0800

    自动摘要(结果不准确) Signed-off-by: huoji555 <huoji566@gmail.com>

--- DataStructure/HanLP/HanLPTest.java
+++ DataStructure/HanLP/HanLPTest.java
@@ -172,6 +172,19 @@ public class HanLPTest {
 
 
 
+    /**
+     *  @author: Ragty
+     *  @Date: 2020/1/4 1:08
+     *  @Description: 总结提取(内部用TextRangeSummary实现)
+     */
+    public static  List<String> summaryExtract(String content, Integer size) {
+        List<String> list = HanLP.extractSummary(content,size);
+        System.out.println(list);
+        return list;
+    }
+
+
+
 
 
 
@@ -217,11 +230,10 @@ public class HanLPTest {
 
        //organizationRecognition("电影院是不错的哦");
 
-       keyWordExtract("程序员(英文Programmer)是从事程序开发、维护的专业人员。一般将程序员分为程序设计人员和程序编码人员，但两者的界限并不非常清楚，特别是在中国。软件从业人员分为初级程序员、高级程序员、系统分析员和项目经理四大类。",5);
+       //keyWordExtract("程序员(英文Programmer)是从事程序开发、维护的专业人员。一般将程序员分为程序设计人员和程序编码人员，但两者的界限并不非常清楚，特别是在中国。软件从业人员分为初级程序员、高级程序员、系统分析员和项目经理四大类。",5);
 
-
-
-
+       //String document = "算法可大致分为基本算法、数据结构的算法、数论算法、计算几何的算法、图的算法、动态规划以及数值分析、加密算法、排序算法、检索算法、随机化算法、并行算法、厄米变形模型、随机森林算法。 算法可以宽泛的分为三类，一，有限的确定性算法，这类算法在有限的一段时间内终止。他们可能要花很长时间来执行指定的任务，但仍将在一定的时间内终止。这类算法得出的结果常取决于输入值。 二，有限的非确定算法，这类算法在有限的时间内终止。然而，对于一个（或一些）给定的数值，算法的结果并不是唯一的或确定的。 三，无限的算法，是那些由于没有定义终止定义条件，或定义的条件无法由输入的数据满足而不终止运行的算法。通常，无限算法的产生是由于未能确定的定义终止条件。";
+       //summaryExtract(document,3);
 
 
     }
